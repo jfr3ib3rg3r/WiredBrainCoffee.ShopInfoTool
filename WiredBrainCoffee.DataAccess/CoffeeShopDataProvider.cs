b79cb221a13e075aefa2d2b8852b22d23643c392
Dipl.-Ing. Joerg M. Freiberger <joerg.freiberger@expert4.net>
2021-05-06 07:15:09 +0200

    Feature 1

--- WiredBrainCoffee.DataAccess/CoffeeShopDataProvider.cs
+++ WiredBrainCoffee.DataAccess/CoffeeShopDataProvider.cs
@@ -1,11 +1,14 @@
+using System.Collections.Generic;
+using WiredBrainCoffee.DataAccess.Model;
+
 namespace WiredBrainCoffee.DataAccess
 {
     public class CoffeeShopDataProvider
     {
         public IEnumerable<CoffeeShop> LoadCoffeeShops() {
-            yield return new CoffeeShop { Location = "Frankfurt", BeansInStickInKg = 107 };
-            yield return new CoffeeShop { Location = "Freiburg", BeansInStickInKg = 23 };
-            yield return new CoffeeShop { Location = "Munich", BeansInStickInKg = 56 };
+            yield return new CoffeeShop { Location = "Frankfurt", BeansInStockInKg = 107 };
+            yield return new CoffeeShop { Location = "Freiburg", BeansInStockInKg = 23 };
+            yield return new CoffeeShop { Location = "Munich", BeansInStockInKg = 56 };
         }
     }
 }

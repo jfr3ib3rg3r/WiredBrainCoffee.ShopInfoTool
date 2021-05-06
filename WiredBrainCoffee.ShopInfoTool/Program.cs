c282ebada1e39d2415aefdb11d03b032524c9a6e
Dipl.-Ing. Joerg M. Freiberger <joerg.freiberger@expert4.net>
2021-05-06 07:31:06 +0200

    changed screen text

--- WiredBrainCoffee.ShopInfoTool/Program.cs
+++ WiredBrainCoffee.ShopInfoTool/Program.cs
@@ -9,12 +9,18 @@ namespace WiredBrainCoffee.ShopInfoTool
         {
             Console.WriteLine("Wired Brain Coffee - Shop Info Tool!");
 
-            Console.WriteLine("Write 'help' to list available commands");
+            Console.WriteLine("Write 'help' to list available coffee shop commands, " +
+                                "write 'quit' to exit...");
 
             var coffeeShopDataProvider = new CoffeeShopDataProvider();
 
             while(true) {
                 var line = Console.ReadLine();
+                if(string.Equals("quit", line, StringComparison.OrdinalIgnoreCase))
+                {
+                    break;
+                }
+
                 var coffeeShops = coffeeShopDataProvider.LoadCoffeeShops();
                 if(string.Equals("help", line, StringComparison.OrdinalIgnoreCase))
                 {

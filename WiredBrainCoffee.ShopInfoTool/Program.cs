82dbdbd2ca815bad5c4742c1e8b447cd11f87d47
Dipl.-Ing. Joerg M. Freiberger <joerg.freiberger@expert4.net>
2021-05-06 07:40:00 +0200

    Feature 2

--- WiredBrainCoffee.ShopInfoTool/Program.cs
+++ WiredBrainCoffee.ShopInfoTool/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using WiredBrainCoffee.DataAccess;
 
 namespace WiredBrainCoffee.ShopInfoTool
@@ -28,6 +29,30 @@ namespace WiredBrainCoffee.ShopInfoTool
                     foreach(var coffeShop in coffeeShops)
                     {
                         Console.WriteLine($"> " + coffeShop.Location);
+                    }
+                }
+                else
+                {
+                    var foundCoffeeShops = coffeeShops
+                                            .Where(x => x.Location.StartsWith(line, StringComparison.OrdinalIgnoreCase))
+                                            .ToList();
+                    if(foundCoffeeShops.Count == 0)
+                    {
+                        Console.WriteLine($"> Command '{line}' not found");
+                    }
+                    else if(foundCoffeeShops.Count == 1)
+                    {
+                        var coffeShop = foundCoffeeShops.Single();
+                        Console.WriteLine($"> Location: {coffeShop.Location}");
+                        Console.WriteLine($"> Beans in stock: {coffeShop.BeansInStockInKg} kg");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"> Multiple matching coffee shop commands found:");
+                        foreach (var coffeeType in foundCoffeeShops)
+                        {
+                            Console.WriteLine($"> {coffeeType.Location}");
+                        }
                     }
                 }
             }

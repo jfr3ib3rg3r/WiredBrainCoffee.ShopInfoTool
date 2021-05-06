2f8105b99221c7c521f768fd0adc1e7ed3c3c558
Dipl.-Ing. Joerg M. Freiberger <joerg.freiberger@expert4.net>
2021-05-06 08:03:14 +0200

    PaperCupsInStock

--- WiredBrainCoffee.DataAccess/Model/CoffeeShop.cs
+++ WiredBrainCoffee.DataAccess/Model/CoffeeShop.cs
@@ -4,5 +4,6 @@ namespace WiredBrainCoffee.DataAccess.Model
     {
         public string Location { get; set; }
         public int BeansInStockInKg { get; set; }
+        public int PaperCupsInStock { get; set; }
     }
 }
